148213eeff1215246fc051fc2dfcf7e8c7202df7
Enrico Franchi <efranchi@ce.unipr.it>
2010-06-18 16:05:19 +0200

    Rough version. Bad design.

---
+++ games/python/tictactoe.py
@@ -0,0 +1,200 @@
+import re
+import itertools as it
+
+import cStringIO
+
+BLANK = 0x0
+NOUGHT = 0x1
+CROSS = 0x2
+
+VALID = set((NOUGHT, CROSS))
+
+SYMBOLS = {
+    BLANK: '_',
+    NOUGHT: 'O',
+    CROSS: 'X'
+}
+
+def _pair_to_index(x_pos, y_pos):
+    return x_pos + 3 * y_pos
+
+def _index_to_pair(pos):
+    pair = divmod(pos, 3)
+    return pair[1], pair[0]
+
+def sure_win(three):
+    return len(set(three)) == 1
+
+def close_win(three):
+    blanks = three.count(BLANK)
+    if blanks > 1:
+        return False
+    elif blank == 1 and three.count(NOUGHT) == 2:
+        return True
+    elif blank == 1 and three.count(CROSS) == 2:
+        return True
+    else:
+        return False
+
+
+
+class Board(object):
+    def __init__(self):
+        self._board = [BLANK, ] * 9
+
+    def __getitem__(self, pos):
+        try:
+            x_pos, y_pos = pos
+        except ValueError:
+            return self._board[pos]
+        else:
+            return self._board[_pair_to_index(x_pos, y_pos)]
+
+
+    def __setitem__(self, pos, val):
+        if val in VALID:
+            try:
+                x_pos, y_pos = pos
+            except ValueError: pass
+            else:
+                pos = _pair_to_index(x_pos, y_pos)
+            if self._board[pos] in VALID:
+                raise ValueError('Already set position')
+            else:
+                self._board[pos] = val
+        else:
+            raise ValueError('Not a valid move')
+
+    def col(self, index):
+        return self._board[index::3]
+
+    def row(self, index):
+        return self._board[3*index:3*index+3]
+
+    def cols(self):
+        return it.imap(self.col, range(3))
+
+    def rows(self):
+        return it.imap(self.row, range(3))
+
+    def diag(self):
+        return [self._board[0], self._board[4], self._board[8]]
+
+    def antidiag(self):
+        return [self._board[6], self._board[4], self._board[2]]
+
+    def threes(self):
+        return it.chain(self.rows(), self.cols(),
+                        (self.diag(), self.antidiag()))
+
+    def winner(self):
+        for three in self.threes():
+            if sure_win(three):
+                return three.pop()
+        else:
+            return None
+
+    def draw(self):
+        '''Nobody can win'''
+        # try to discover draws asap
+        return BLANK not in self._board
+
+    def __str__(self):
+        sio = cStringIO.StringIO()
+        for row in self.rows():
+            print >>sio, SYMBOLS[row[0]], SYMBOLS[row[1]], SYMBOLS[row[2]]
+        sio.seek(0)
+        return sio.read()
+
+
+class Player(object):
+    def __init__(self, symbol, strategy):
+        self.set_strategy(strategy)
+        assert symbol in VALID
+        self.strategy.symbol = symbol
+
+    def set_strategy(self, new_strategy):
+        try:
+            old_strategy = self.strategy
+        except AttributeError:
+            self.strategy = new_strategy
+        else:
+            self.strategy = new_strategy
+            self.strategy.symbol = old_strategy.symbol
+
+    def notify_move(self, move):
+        try:
+            notifier = self.strategy.notify_move
+        except AttributeError:
+            pass
+        else:
+            notifier(move)
+
+    @property
+    def symbol(self):
+        return self.strategy.symbol
+
+    def next(self):
+        return self.strategy.next()
+
+class AskTheHuman(object):
+    rex = re.compile('(\d+),\s+(\d+)')
+    def next(self):
+        while 1:
+            line = raw_input('%s> ' % self.symbol)
+            match = self.rex.match(line)
+            if match:
+                return tuple(int(g) for g in match.groups())
+            else:
+                try:
+                    return _index_to_pair(int(line))
+                except ValueError:
+                    pass
+
+class Game(object):
+    def __init__(self, *args):
+        self.board = Board()
+        self.players = args
+        self.moves = 0
+
+    def next_move(self):
+        player = self.players[self.moves % 2]
+        move = player.next()
+        return (move, player.symbol)
+
+    def perform_move(self, move):
+        self.board.__setitem__(*move)
+        self.moves += 1
+
+    def notify_move(self, move):
+        for p in self.players:
+            p.notify_move(move)
+
+    def play(self):
+        while 1:
+            print self.board
+            if self.board.draw():
+                print 'DRAW!'
+                break
+            possible_winner = self.board.winner()
+            if possible_winner:
+                print 'Player %s won!' % possible_winner
+                break
+            for i in range(3):
+                move = self.next_move()
+                try:
+                    self.perform_move(move)
+                except ValueError:
+                    print 'Invalid move!'
+                else:
+                    self.notify_move(move)
+                    break
+            else:
+                print 'TOO MANY INVALID MOVES'
+                break
+
+if __name__ == '__main__':
+    game = Game(Player(NOUGHT, AskTheHuman()),
+                Player(CROSS, AskTheHuman()))
+    game.play()
+
