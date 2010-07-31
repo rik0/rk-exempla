d05f4fe363fee05744d26680d0c3a957044baba8
Enrico Franchi <efranchi@ce.unipr.it>
2010-07-31 23:05:48 +0200

    Added license and module comment to make pylint happy. Disabled unavoidable warning and info messages.

--- filesystem/python/atomic_file.py
+++ filesystem/python/atomic_file.py
@@ -1,9 +1,14 @@
+# Enrico Franchi (c) 2010
+# This module is released under MIT license.
+
+'''Simple module containing a file-like object whose behavior is
+to actually save the file only when closed. Before that, data is
+stored in a temporary file: thus when creating a file, if another file
+with the same name exists that is never substituted with a partial
+file.'''
+
 import os
-import sys
 import tempfile
-
-
-
 
 class AtomicFile(object):
     '''A file-like object where writes are committed only when closed.
@@ -12,8 +17,12 @@ class AtomicFile(object):
     On close, the temporary is renamed according to the name parameter.
 
     AtomicFiles are meant to be used with context managers.'''
-    def __init__(self, name, mode=None, bufsize=-1, suffix='', prefix='', dir=None):
-        mode = 'r+b' if mode is None else mode
+
+    # Unfortunately the TemporaryFile interface uses a dir parameter
+    # pylint: disable-msg=R0913
+    # pylint: disable-msg=W0622
+    def __init__(self, name, bufsize=-1, suffix='', prefix='', dir=None):
+        mode = 'r+b'
         self.name = name
         self.tempfile = tempfile.NamedTemporaryFile(
             mode=mode, bufsize=bufsize, suffix=suffix, prefix=prefix,
@@ -22,7 +31,7 @@ class AtomicFile(object):
     def __enter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, _exc_type, _exc_val, _exc_tb):
         self.swap()
         return False
 
