579b3da46365b79b041fd2a75abadf7daebd0dce
Enrico Franchi <efranchi@ce.unipr.it>
2010-10-13 21:20:26 +0200

    Added comments. Fixed bug.

--- algorithms/python/wordlists.py
+++ algorithms/python/wordlists.py
@@ -12,6 +12,7 @@ import itertools as it
 from os import path
 
 class DownloadReport(object):
+    '''Helper class to be used as urlretrieve hook.'''
     def __init__(self):
         self.downloaded = 0
     def __call__(self, blocks, block_size, total_size):
@@ -23,6 +24,8 @@ class DownloadReport(object):
         print
 
 def get_dictionary_file(url, pathname):
+    '''Download tarfile from url into a file named according to url basename
+    and expands the file into pathname.'''
     filename = path.basename(url)
     if not path.isdir(pathname):
         os.mkdir(pathname)
@@ -33,6 +36,7 @@ def get_dictionary_file(url, pathname):
     tararchive.extractall(pathname)
 
 def check_dictionary_file(pathname):
+    '''Check if the dictionary file has been expanded into pathname'''
     return (path.isdir(pathname) and
             path.isfile(path.join(pathname, 'length02.txt')))
 
@@ -54,6 +58,14 @@ def parts_array(max_sum, no_parts):
     return parts
 
 def read_all(dict_dir, no_words, min_length, max_length, lengths, predicate):
+    '''Read words from a dictionary.
+
+    dict_dir is the directory where the dictionary is located
+    no_words is the maximum number of words (or None for all) to be read
+    min_length/max_length specify the range of the lengths of words to be read
+    alternatively, lengths is an iterable containing the lengths (3, 4, 7)
+    only words which make predicate true are returned
+    '''
     if not lengths:
         lengths = set(range(min_length, max_length+1))
     else:
@@ -68,16 +80,27 @@ def read_all(dict_dir, no_words, min_length, max_length, lengths, predicate):
     return words
 
 def read_file(filename, no_words, predicate):
+    '''Read at most no_words satisfying predicate from filename.
+
+    If no_words > of the number of words in filename, all words are returned.
+    If no_words is None, all words are returned.'''
     with open(filename) as fh:
         all_words = fh.read().split('\n')
-        if no_words and no_words < len(all_words):
-            selected_words = random.sample(all_words, no_words)
+        filtered_words = filter(predicate, (w.strip() for w in all_words))
+        if no_words and no_words < len(filtered_words):
+            selected_words = random.sample(filtered_words, no_words)
         else:
-            selected_words = all_words
-        return [w for w in (w.strip() for w in selected_words) if predicate(w)]
+            selected_words = filtered_words
+        return selected_words
 
 def get_wordlist(pathname, backup_url, no_words=None, min_length=2,
                  max_length=32, lengths=None, predicate=(lambda x: True)):
+    '''Return a list of words.
+
+    pathname is the place where the dictionary should be stored
+    backup_url is used to download the dictinary to pathname if not present
+    the other arguments have the same meaning than in read_all
+    '''
     if not check_dictionary_file(pathname):
         get_dictionary_file(backup_url, pathname)
     return read_all(pathname, no_words, min_length,
