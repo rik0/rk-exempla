43386cba496961373f16ddd728d32c7bc2f71d1a
Enrico Franchi <efranchi@ce.unipr.it>
2010-10-18 13:59:32 +0200

    Match n occurrence of regex.

---
+++ misc/python/match_nth.py
@@ -0,0 +1,20 @@
+import re, sre
+
+
+class Nth(object):
+    def __init__(self, repl, n=0):
+        self.n = n
+        self.repl = repl
+    def __call__(self, match):
+        if self.n == 0:
+            self.n -= 1
+            return match.expand(self.repl)
+        else:
+            self.n -= 1
+            return match.group(0)
+
+
+
+rex = re.compile('a(\w+)')
+
+print rex.sub(Nth(r'A\1', 1), "aardvark abbot abide")
